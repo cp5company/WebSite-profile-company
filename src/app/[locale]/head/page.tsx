acd8ce2b181cf251481673ef64540e20e9c00c52
cp5company <pdell9846@gmail.com>
2024-11-13 19:07:24 +0300

    Update page.tsx

--- src/app/[locale]/head/page.tsx
+++ src/app/[locale]/head/page.tsx
@@ -1,6 +1,4 @@
-// Header Component
 "use client";
-
 import React from "react";
 import {
   FaFacebookF,
@@ -19,33 +17,31 @@ const Header: React.FC<{ headerdata: ContentDataTitle }> = ({ headerdata }) => {
 
   return (
     <header className="relative p-4 flex items-center justify-center overflow-hidden">
-    <div className="absolute inset-0 from-primary to-accent opacity-90" />
-  
-    <div className="relative z-10 text-center text-primary-foreground px-4 sm:px-6 lg:px-8">
-      <h1 className="text-4xl md:text-6xl text-black dark:text-white font-bold tracking-tight mb-8 animate-fade-in">
-        {headerdata.title}
-      </h1>
-  
-      <h2 className="text-2xl md:text-4xl text-black dark:text-white font-semibold mb-12 animate-slide-up">
-        {headerdata.body}
-      </h2>
-      <a
-        href="mailto:pdell9846@gmail.com"
-        className="bg-primary hover:bg-primary/90 border border-solid border-black text-primary-foreground px-8 py-4 rounded-[var(--radius)] text-lg font-semibold transition-all duration-300 transform hover:scale-105"
-      >
-        Let&apos;s Discuss Your Project
-      </a>
-  
-      <div className="flex justify-center space-x-6 mt-16">
-        <SocialIcon Icon={FaFacebookF} href="#" />
-        <SocialIcon Icon={FaInstagram} href="#" />
-        <SocialIcon Icon={FaLinkedinIn} href="#" />
-        <SocialIcon Icon={FaTimes} href="#" />
+      <div className="absolute inset-0 from-primary to-accent opacity-90" />
+      <div className="relative z-10 text-center text-primary-foreground px-4 sm:px-6 lg:px-8">
+        <h1 className="text-4xl md:text-6xl text-black dark:text-white font-bold tracking-tight mb-8 animate-fade-in">
+          {headerdata.title}
+        </h1>
+        <h2 className="text-2xl md:text-4xl text-black dark:text-white font-semibold mb-12 animate-slide-up">
+          {headerdata.body}
+        </h2>
+        <a
+          href="mailto:pdell9846@gmail.com"
+          className="bg-primary hover:bg-primary/90 border border-solid border-black text-primary-foreground px-8 py-4 rounded-[var(--radius)] text-lg font-semibold transition-all duration-300 transform hover:scale-105"
+        >
+          Let&apos;s Discuss Your Project
+        </a>
+        <div className="flex justify-center space-x-6 mt-16">
+          <SocialIcon Icon={FaFacebookF} href="#" />
+          <SocialIcon Icon={FaInstagram} href="#" />
+          <SocialIcon Icon={FaLinkedinIn} href="#" />
+          <SocialIcon Icon={FaTimes} href="#" />
+        </div>
       </div>
-    </div>
-  </header>  
+    </header>
   );
 };
+
 interface SocialIconType {
   Icon: React.FC<{ className?: string }>;
   href: string;
